eeae75e570197fbf9643fa4ab349ac788feb0940
楊子逸 <Brian@yangziyis-MacBook-Air.local>
2017-01-25 19:29:19 +0800

    Initial Commit

---
+++ sum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sum
+//
+//  Created by 楊子逸 on 25/01/2017.
+//  Copyright © 2017 楊子逸. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
